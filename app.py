d74049f849f91c72135dbc58b24fba7bd130d2ca
sumikk <sumithskk@gmail.com>
2023-07-17 18:58:51 +0530

    Gitignore

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-import os, io
+import io
+import os
 import streamlit as st
-import webbrowser
 
 
 def redirect_to_url(url):
     js = f"window.location.href='{url}'"
     html = f"<script>{js}</script>"
     st.markdown(html, unsafe_allow_html=True)
+
+
+def create_ip_file(datafiles):
+    if datafiles:
+        for datafile in datafiles:
+            if datafile is not None:
+                file_path = os.path.join("inputs", f"{datafile.name}")
+                with open(file_path, "wb") as f:
+                    f.write(datafile.read())
 
 
 def main():
@@ -29,36 +38,76 @@ def main():
     )
 
     st.title("Langchain to Langflow")
+    os.makedirs('inputs', exist_ok=True)
+    # input
+    # key = str(uuid.uuid4())
+    key = 0
+
+    #
+    # st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>',
+    #          unsafe_allow_html=True)
+    # st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>',
+    #          unsafe_allow_html=True)
+
+    anyinput = st.radio("Any input files for Langchain Code",
+                        ("No", "Yes"))
+
     left_column, right_column = st.columns(2)
+
     with left_column:
-        input_file = st.file_uploader("Upload your Langchain File", type="py")
-    if input_file is not None:
-        with io.TextIOWrapper(input_file, encoding="utf-8", newline="") as file_wrapper:
-            file_contents = file_wrapper.read()
+        input_file = st.file_uploader("Upload Your Langchain File", type="py")
 
-        # Create a new file with the same contents
-        with open("input1.py", "w", newline="") as new_file:
-            new_file.write(file_contents)
-            temp_file_path = new_file.name
-            with right_column:
-                st.write("")
-                st.write("")
-                st.code(f"File Name: {input_file.name}")
-                st.code(f"File Size: {len(file_contents)} bytes")
-        container = st.sidebar.container()
-        with container:
-            st.text_area(
-                f"{input_file.name}",
-                file_contents,
-                height=400,
-                key="readonly_textarea",
-                disabled=True,
-            )
-        run_main()
-        download_json()
+    # if anyinput == 'Yes':
+    #     with right_column:
+    #         datafiles = st.file_uploader('Upload additional input files ',
+    #                                      accept_multiple_files=True)
+    #         #create_ip_file(datafiles)
+
+    if anyinput == 'No':
+        if input_file is not None:
+            run_all_file(input_file)
+    elif anyinput == 'Yes':
+        with right_column:
+            datafiles = st.file_uploader('Upload additional input files ',
+                                         accept_multiple_files=True)
+        if len(datafiles) != 0:
+            create_ip_file(datafiles)
+            run_all_file(input_file)
+    #
+    # if input_file is not None:
+    #     if (anyinput == 'Yes') and (len(datafiles)!=0):
+    #         create_ip_file(datafiles)
+    #         run_all_file(input_file)
+    #     elif anyinput == 'No':
+    #         run_all_file(input_file)
 
 
+def run_all_file(input_file):
+    with io.TextIOWrapper(input_file, encoding="utf-8", newline="") as file_wrapper:
+        file_contents = file_wrapper.read()
 
+    # Create a new file with the same contents
+    with open("qwerty1234567890.py", "w", newline="") as new_file:
+        new_file.write(file_contents)
+        # with right_column:
+        #     st.write("")
+        #     st.write("")
+        #     st.code(f"File Name: {input_file.name}")
+        #     st.code(f"File Size: {len(file_contents)} bytes")
+    container = st.sidebar.container()
+    with container:
+        st.text_area(
+            f"{input_file.name}",
+            file_contents,
+            height=400,
+            key="readonly_textarea",
+            disabled=True,
+        )
+    run_main()
+    download_json()
+
+
+# @st.cache_resource
 def run_main():
     with open("main.py", "r") as file:
         file_contents = file.read()
@@ -74,11 +123,11 @@ def download_json():
         max-width: 10% !important;
     },
     .title {
-        font-size: 10px;
+        font-size: 20px;
     }
     </style>
 
-    <h2 class="title">Download Json File</h2>
+    <h1 class="title">Download Json File</h1>
     """,
         unsafe_allow_html=True,
     )
